9265b98b7a37cc3d6a91e5f82ddb0a008664cf58
Tayná <taynasuperti@gmail.com>
2025-08-25 17:43:47 -0300

    adicionando o gitignore

---
+++ SOLID/Desafio-SOLID/SRP/Solucao/PedidoBom.cs
@@ -0,0 +1,39 @@
+﻿// SRP (Single Responsibility Principle): nesse princípio, é falado que uma classe deve ter um único motivo para mudar, ela deve ter apenas uma responsabilidade  
+
+namespace Desafio_SOLID.SRP.Solucao
+{
+    //aqui, cada classe tem a sua responsabilidade única
+    internal class PedidoBom
+    {
+        //declarando os dados do pedido
+        public int Id { get; set; }
+        public decimal ValorItens { get; set; }
+        public decimal ValorFrete { get; set; }
+
+        public decimal CalcularValorFinal()
+        {
+            //aqui vem a lógica para calcular o valor final do item (valor + frete)
+            return ValorItens + ValorFrete;
+        }
+    }
+
+    internal class SalvarPedido
+    {
+        public void SalvarNoBanco(int id)
+        {
+            //aqui vem a lógica pra salvar o pedido no banco
+            //simulação de como seria a conexão com o banco de dados
+            Console.WriteLine($"[SRP Solução] Salvando pedido {id} no banco...");
+        }
+    }
+
+    internal class EnviarEmail
+    {
+        public void EnviarEmailConfirmacao(int id)
+        {
+            //aqui vem a lógica pra enviar email de confirmação da compra
+            //simulação de como seria o envio de email
+            Console.WriteLine($"[SRP Solução] Enviando e-mail de confirmação do pedido {id}...");
+        }
+    }
+}
---
+++ SOLID/Desafio-SOLID/SRP/Violacao/PedidoRuim.cs
@@ -0,0 +1,33 @@
+﻿// SRP (Single Responsibility Principle): nesse princípio, é falado que uma classe deve ter um único motivo para mudar, ela deve ter apenas uma responsabilidade  
+
+namespace Desafio_SOLID.SRP.Violacao
+{
+    //declarando os dados do pedido
+    internal class PedidoRuim
+    {
+        public int Id { get; set; }
+        public decimal ValorItens { get; set; }
+        public decimal ValorFrete { get; set; }
+
+        //essa clase faz "de tudo", ela faz a regra de negócio, faz a persistência e envia email
+        public decimal CalcularValorFinal()
+        {
+            //aqui vem a lógica para calcular o valor final do item (valor + frete)
+            return ValorItens + ValorFrete;
+        }
+
+        public void SalvarNoBanco()
+        {
+            //aqui vem a lógica pra salvar o pedido no banco
+            //simulação de como seria a conexão com o banco de dados
+            Console.WriteLine($"[SRP Violação] Salvando pedido {Id} no banco...");
+        }
+
+        public void EnviarEmailConfirmacao()
+        {
+            //aqui vem a lógica pra enviar email de confirmação da compra
+            //simulação de como seria o envio de email
+            Console.WriteLine($"[SRP Violação] Enviando e-mail do pedido {Id}...");
+        }
+    }
+}
