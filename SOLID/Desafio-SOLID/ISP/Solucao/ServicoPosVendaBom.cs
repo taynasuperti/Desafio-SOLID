2827aa238024e8367b23214b28fda3df955da55c
Tayná <taynasuperti@gmail.com>
2025-08-26 12:41:03 -0300

    finalização dos exemplos de violação e solução do quarto princípio (isp) de solid

---
+++ SOLID/Desafio-SOLID/ISP/Solucao/ServicoPosVendaBom.cs
@@ -0,0 +1,57 @@
+﻿// ISP (Interface Segregation Principle): nesse princípio, é falado que nenhuma classe deve ser forçada a implementar métodos que não utiliza
+
+namespace Desafio_SOLID.ISP.Solucao
+{
+    internal class ServicoPosVendaBom
+    {
+        //aqui, cada serviço tem uma interface separada
+        internal interface IEmailConfirmacao
+        {
+            void EnviarEmailConfirmacao();
+        }
+
+        internal interface IInstalacao
+        {
+            void AgendarInstalacao();
+        }
+
+        internal interface IGarantia
+        {
+            void RegistrarGarantia();
+        }
+
+        //o produto físico precisa de todos os serviços
+        internal class ProdutoFisico: IEmailConfirmacao, IInstalacao, IGarantia
+        {
+            public void EnviarEmailConfirmacao()
+            {
+                //simulação de envio de e-mail
+                Console.WriteLine("[ISP Solução] Enviando e-mail de confirmação...");
+            }
+            public void AgendarInstalacao()
+            {
+                //simulação de agendamento de instalação
+                Console.WriteLine("[ISP Solução] Agendando instalação do produto...");
+            }
+            public void RegistrarGarantia()
+            {
+                //simulação de registro de garantia
+                Console.WriteLine("[ISP Solução] Registrando garantia do produto...");
+            }
+        }
+        //o produto digital implementa apenas o que precisa
+        internal class ProdutoDigital: IEmailConfirmacao, IGarantia
+        {
+            public void EnviarEmailConfirmacao()
+            {
+                //simulação de envio de e-mail
+                Console.WriteLine("[ISP Solução] Enviando e-mail de confirmação...");
+            }
+            public void RegistrarGarantia()
+            {
+                //simulação de registro de garantia
+                Console.WriteLine("[ISP Solução] Registrando garantia do produto...");
+            }
+        }
+    }
+}
---
+++ SOLID/Desafio-SOLID/ISP/Violacao/ServicoPosVendaRuim.cs
@@ -0,0 +1,59 @@
+﻿// ISP (Interface Segregation Principle): nesse princípio, é falado que nenhuma classe deve ser forçada a implementar métodos que não utiliza
+
+namespace Desafio_SOLID.ISP.Violacao
+{
+    internal class ServicoPosVendaRuim
+    {
+        //aqui representa uma interface que define vários serviços pós-venda
+        interface IServicoPosVenda
+        {
+            void EnviarEmailConfirmacao();
+            void AgendarInstalacao();
+            void RegistrarGarantia();
+        }
+
+        //o produto físico precisa de todos os serviços
+        internal class ProdutoFisico: IServicoPosVenda
+        {
+            public void EnviarEmailConfirmacao()
+            {
+                //simulação de envio de e-mail
+                Console.WriteLine("[ISP Violação] Enviando e-mail de confirmação...");
+            }
+
+            public void AgendarInstalacao()
+            {
+                //simulação de agendamento de instalação
+                Console.WriteLine("[ISP Violação] Agendando instalação do produto...");
+            }
+
+            public void RegistrarGarantia()
+            {
+                //simulação de registro de garantia
+                Console.WriteLine("[ISP Violação] Registrando garantia do produto...");
+            }
+        }
+
+        //aqui vem a violação desse princípio: produto digital não precisa de instalação, mas é forçado a implementar o método
+        internal class ProdutoDigital : IServicoPosVenda
+        {
+            public void EnviarEmailConfirmacao()
+            {
+                //simulação de envio de e-mail
+                Console.WriteLine("[ISP Violação] Enviando e-mail de confirmação...");
+            }
+
+            public void AgendarInstalacao()
+            {
+                // esse método não faz sentido para produto digital
+                Console.WriteLine("[ISP Violação] Produto digital não precisa de instalação!");
+            }
+
+            public void RegistrarGarantia()
+            {
+                //simulação de registro de garantia
+                Console.WriteLine("[ISP Violação] Registrando garantia do produto...");
+            }
+        }
+    }
+}
