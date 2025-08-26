dbc5e01cb06a44ea4eab1dfad2b3589c5540a43d
Tayná <taynasuperti@gmail.com>
2025-08-26 11:01:54 -0300

    finalização dos exemplos de violação e solução do segundo princípio (ocp) de solid

---
+++ SOLID/Desafio-SOLID/OCP/Solucao/DescontoBom.cs
@@ -0,0 +1,39 @@
+﻿// OCP (Open Closed Principle): nesse princípio, é falado que as classes devem ser abertas para extensão, mas fechadas para modificação
+// resumindo, é possível adicionar coisas novas a uma classe, mas não deve mudar o que já está funcionando nela.
+
+namespace Desafio_SOLID.OCP.Solucao
+{
+    //usei o abstract para que essa classe não possa ser instanciada diretamente
+    //(não deixa criar um objeto diretamente da classe abstrata)
+    internal abstract class DescontoCliente
+    {
+        public abstract double CalcularDescontoCliente();
+    }
+
+    class DescontoClienteComum : DescontoCliente //indica a herança da class 
+    {
+        public override double CalcularDescontoCliente()
+        //o override serve para substituir a versão do método que veio da classe base
+        {
+            return 0.05; //5% de desconto
+        }
+    }
+
+    class DescontoClienteVIP : DescontoCliente //indica a herança da class 
+    {
+        public override double CalcularDescontoCliente()
+        {
+            return 0.1; //10% de desconto
+        }
+    }
+
+    internal class DescontoBom
+    {
+        //aqui, cada cliente tem a sua própria classe
+        //se for necessário adicionar um novo tipo de cliente, não é necessário alterar essa classe
+        public double AplicarDesconto(DescontoCliente cliente)
+        {
+            return cliente.CalcularDescontoCliente();
+        }
+    }
+}
---
+++ SOLID/Desafio-SOLID/OCP/Violacao/DescontoRuim.cs
@@ -0,0 +1,32 @@
+﻿// OCP (Open Closed Principle): nesse princípio, é falado que as classes devem ser abertas para extensão, mas fechadas para modificação
+// resumindo,é possível adicionar coisas novas a uma classe, mas não deve mudar o que já está funcionando nela.
+
+namespace Desafio_SOLID.OCP.Violacao
+{
+    internal class DescontoRuim
+    {
+        //o erro aqui é  por toda vez que for criado um novo tipo de cliente para calcular o desconto, a classe vai precisar ser alterada novamente
+        public double CalcularDescontoCliente(string TipoCliente)
+        {
+            //esse tipo de ação pode "quebrar" o código se usada em grandes sistemas
+            if(TipoCliente == "Comum")
+            {
+                //simulação de como seria a exposição do desconto
+                Console.WriteLine($"[OCP Violação] Desconto aplicado: {CalcularDescontoCliente("Comum")}");
+                return 0.05; //5% de desconto
+            } else if (TipoCliente == "VIP")
+            {
+                //simulação de como seria a exposição do desconto
+                Console.WriteLine($"[OCP Violação] Desconto aplicado: {CalcularDescontoCliente("VIP")}");
+                return 0.1; //10% de desconto
+
+            }
+            else
+            {
+                //simulação de como seria a exposição do desconto
+                Console.WriteLine($"[OCP Violação] Desconto aplicado: {CalcularDescontoCliente("0")}");
+                return 0; //sem desconto
+            }
+        }
+    }
+}
