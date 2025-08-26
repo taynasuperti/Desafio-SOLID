2d1e1a0cb30cd41938366fabca4b05aa3bcff80c
Tayná <taynasuperti@gmail.com>
2025-08-26 11:56:29 -0300

    finalização dos exemplos de violação e solução do terceiro princípio (lsp) de solid

---
+++ SOLID/Desafio-SOLID/LSP/Solucao/EntregaBoa.cs
@@ -0,0 +1,46 @@
+﻿// LSP (Liskov Substitution Principle): nesse princípio, é falado que uma subclasse pode substituir a classe base sem alterar o funcionamento correto do projeto
+// resumindo, se uma classe filha não consegue cumprir a mesma funcionalidade da classe pai, ela está violando esse princípio
+// caso algo dê errado, a classe filha deve substituir e cumprir todas as "responsabilidades" da classe pai
+
+namespace Desafio_SOLID.LSP.Solucao
+{
+    //aqui, cada tipo de entrega define seu próprio comportamento 
+    //desse modo, o programa não "quebra" porque as classes filhas têm seu próprio comportamento
+    internal class EntregaBoa
+    {
+        public int IdPedido { get; set; }
+        public string MetodoEntrega { get; set; } = string.Empty;
+
+        
+        public void ExibirDados()
+        {
+            //simulação da exibição dos dados da entrega
+            Console.WriteLine($"[LSP Solução] Pedido {IdPedido} será entregue via: {MetodoEntrega}");
+        }
+    }
+
+    //usei o abstract para que essa classe não possa ser instanciada diretamente
+    //(não deixa criar um objeto diretamente da classe abstrata)
+    internal abstract class TipoEntrega
+    {
+        public abstract void ProcessarEntrega();
+    }
+
+    internal class EntregaCorreios : TipoEntrega
+    {
+        public override void ProcessarEntrega()
+        {
+            //simulação de envio pelos correios
+            Console.WriteLine("[LSP Solução] Enviando pelos Correios. Frete: R$20,00");
+        }
+    }
+
+    internal class RetiradaNaLoja : TipoEntrega
+    {
+        public override void ProcessarEntrega()
+        {
+            //simulação de retirada na loja
+            Console.WriteLine("[LSP Solução] Cliente vai retirar na loja. Sem frete.");
+        }
+    }
+}
---
+++ SOLID/Desafio-SOLID/LSP/Violacao/EntregaRuim.cs
@@ -0,0 +1,37 @@
+﻿// LSP (Liskov Substitution Principle): nesse princípio, é falado que uma subclasse pode substituir a classe base sem alterar o funcionamento correto do projeto
+// resumindo, se uma classe filha não consegue cumprir a msm funcionalidade da classe pai, ela está violando esse princípio
+//caso algo dê errado, a classe filha deve subistituir e cumprir todas as "responsabilidades" da classe pai
+
+namespace Desafio_SOLID.LSP.Violacao
+{
+    internal class EntregaRuim
+    {
+        //aqui é criado um método genérico para calcular o valor do frete
+        public virtual decimal CalcularFrete()
+        // o virtual permite que o método seja sobrescrito por uma classe filha
+        {
+            return 20.0m;
+        }
+    }
+    class EntregaCorreiosRuim : EntregaRuim
+    {
+        //frete padrão dos correios
+        public override decimal CalcularFrete()
+        //o override serve para substituir a versão do método que veio da classe base
+        {
+            Console.WriteLine("[LSP Violação] Frete via Correios aplicado: R$20,00");
+            return 20.0m;
+        }
+    }
+    internal class RetiradaNaLojaRuim : EntregaRuim
+    {
+        //a violação desse princípo vem aqui:
+        // essa classe não consegue cumprir o contrato da classe base (o pai calcula apenas frete, não existe funcionalidade nele pra retirada física do produto)
+        // o projeto "quebra" pq a herança não consegue usar o método CalcularFrete()
+        public override decimal CalcularFrete()
+        {
+            //simulação de erro ao tentar calcular frete para retirada
+            throw new Exception("[LSP Violação] Retirada na loja não possui frete!");
+        }
+    }
+}
