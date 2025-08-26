c5d8d21f39aa6988c7edd5e486e2b95a41aaed42
Tayná <taynasuperti@gmail.com>
2025-08-26 13:00:53 -0300

    finalização dos exemplos de violação e solução do quinto princípio (dip) de solid

---
+++ SOLID/Desafio-SOLID/DIP/Solucao/PagamentoBom.cs
@@ -0,0 +1,47 @@
+﻿// DIP (Dependency Inversion Principle): nesse princípio, é falado que módulos de alto nível não devem depender de módulos de baixo nível
+// ambos devem depender de abstrações. além disso, abstrações não devem depender de detalhes, os detalhes devem depender das abstrações
+
+namespace Desafio_SOLID.DIP.Solucao
+{
+    //aqui, é criada a interface para o pagamento
+    internal interface IPagamento
+    {
+        void Pagar();
+    }
+
+    //implementação concreta para pagamento via boleto
+    internal class PagamentoComBoleto : IPagamento
+    {
+        public void Pagar()
+        {
+            Console.WriteLine("[DIP Solução] Pagamento com boleto realizado.");
+        }
+    }
+
+    //implementação concreta para pagamento via cartão
+    internal class PagamentoComCartao : IPagamento
+    {
+        public void Pagar()
+        {
+            Console.WriteLine("[DIP Solução] Pagamento com cartão realizado.");
+        }
+    }
+
+    //classe principal que representa o pagamento de um pedido
+    internal class PagamentoBom
+    {
+        private readonly IPagamento _pagamento;
+
+        //aqui, a dependência é invertida, a classe depende de uma abstração
+        //aqui, a classe principal vem abaixo para respeitar a ordem de dependência
+        public PagamentoBom(IPagamento pagamento)
+        {
+            _pagamento = pagamento;
+        }
+
+        public void PagarPedido()
+        {
+            _pagamento.Pagar();
+        }
+    }
+}
---
+++ SOLID/Desafio-SOLID/DIP/Violacao/PagamentoRuim.cs
@@ -0,0 +1,27 @@
+﻿// DIP (Dependency Inversion Principle): nesse princípio, é falado que módulos de alto nível não devem depender de módulos de baixo nível
+// ambos devem depender de abstrações. além disso, abstrações não devem depender de detalhes, os detalhes devem depender das abstrações
+
+namespace Desafio_SOLID.DIP.Violacao
+{
+    //essa classe representa o pagamento de um pedido
+    internal class PagamentoRuim
+    {
+        //a violação desse princípio vem aqui, pq a classe depende diretamente de uma implementação concreta (Boleto)
+        private Boleto boleto = new Boleto();
+
+        public void PagarPedido()
+        {
+            //simulação de pagamento com boleto
+            boleto.Pagar();
+        }
+    }
+
+    //esse é o erro, a classe acima só funciona pq essa classe concreta realiza o pagamento via boleto
+    internal class Boleto
+    {
+        public void Pagar()
+        {
+            Console.WriteLine("[DIP Violação] Pagamento com boleto realizado.");
+        }
+    }
+}
